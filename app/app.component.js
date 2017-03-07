13b8102595c9325b7bc3f6f8dfd2f5e45ca631b3
Chirag Sane <chiragsane333@gmail.com>
2017-03-07 18:08:24 +0530

    added sidenav and other icons

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>"
+        templateUrl: './app/app.component.html',
+        styleUrls: ['./app/app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
