0bc9b90d30102fc3a2622d1db8802b35ac6886fa
Chirag Sane <chiragsane333@gmail.com>
2017-03-07 11:45:30 +0530

    added clarity components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from 'clarity-angular';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+                  ClarityModule.forRoot() ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
